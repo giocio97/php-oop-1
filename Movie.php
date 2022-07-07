f5af4337d8723298ddb4119cdf720d48d2b94c48
Giovanni Ciotta <giociotta@gmail.com>
2022-07-07 18:00:17 +0200

    stampo html

--- Movie.php
+++ Movie.php
@@ -4,12 +4,23 @@ class Movie{
     public $anno;
     public $regista;
     public $cast;
+    public $eta;
 
-    public function __construct(string $titolo, int $anno, string $regista, string $cast )
+    public function __construct(string $titolo, int $anno, string $regista, string $cast, int $eta )
     {
         $this ->titolo = $titolo;
         $this ->anno = $anno;
         $this ->regista = $regista; 
-        $this ->cast = $cast;  
+        $this ->cast = $cast;
+        $this -> eta = $eta;  
     }
+    public function controlloeta (){
+        if($this-> eta >= 18){
+            return 'questo è un film per maggiori di 18 anni ';
+        } else {
+            return 'questo è un film per bambini';
+        }
+    }
+
+
 }
--- index.php
+++ index.php
@@ -1,8 +1,35 @@
 <?php
 include __DIR__ . '/Movie.php';
 
-$pulpfiction = new Movie('Pulp Fiction', 1994, 'Quentin Tarantino','John Travolta, Samuel L. Jackson, Uma Thurman, Bruce Willis, ' );
-var_dump($pulpfiction);
+$pulpfiction = new Movie('Pulp Fiction', 1994, 'Quentin Tarantino','John Travolta, Samuel L. Jackson, Uma Thurman, Bruce Willis, ', 18 );
+$pulArray = (array) $pulpfiction;
 
-$forrestgump = new Movie('Forrest Gump', 1994, 'Robert Zemeckis', ' Tom Hanks, Robin Wright, Gary Sinise, Mykelti Williamson, Sally Field ');
-var_dump($forrestgump); 
+
+$forrestgump = new Movie('Forrest Gump', 1994, 'Robert Zemeckis', ' Tom Hanks, Robin Wright, Gary Sinise, Mykelti Williamson, Sally Field ', 13);
+$forrestArray = (array) $forrestgump;
+?> 
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Movie</title>
+</head>
+<body> <?php
+foreach($pulpfiction as $key){
+    ?> <ul>
+        <li><?php echo $key ?></li>
+    </ul><?php
+}
+?> <br> <?php
+foreach ($forrestgump as $forrkey){
+    ?> <ul>
+    <li><?php echo $forrkey ?></li>
+</ul><?php
+}
+
+?>
+</body>
+</html>
